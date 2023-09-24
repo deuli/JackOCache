c5e359305ae9065c13f5c0dc7d5d258b845ec320
deuli <dacostaluca@hotmail.com>
2023-09-24 21:21:38 +0200

    Recipe DataGen

---
+++ src/main/java/deuli/spookyjam2023/datagen/ModRecipeProvider.java
@@ -0,0 +1,37 @@
+package deuli.spookyjam2023.datagen;
+
+import deuli.spookyjam2023.init.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+
+import java.util.function.Consumer;
+
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+
+    public ModRecipeProvider(PackOutput pOutput) {
+        super(pOutput);
+    }
+
+    @Override
+    protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+        jackOLantern(ModBlocks.SINISTER_PUMPKIN.get(), ModBlocks.LIT_SINISTER_PUMPKIN.get(), pWriter);
+        jackOLantern(ModBlocks.GENERIC_PUMPKIN.get(), ModBlocks.LIT_GENERIC_PUMPKIN.get(), pWriter);
+    }
+
+    private void jackOLantern(Block pumpkin, Block jackOLantern, Consumer<FinishedRecipe> pWriter)
+    {
+        ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, jackOLantern)
+                .pattern("A")
+                .pattern("B")
+                .define('A', pumpkin)
+                .define('B', Items.TORCH)
+                .unlockedBy(getHasName(pumpkin), has(pumpkin))
+                .save(pWriter);
+    }
+}
